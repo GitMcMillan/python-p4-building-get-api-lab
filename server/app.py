d9f31188c9fa3acd0430e4e5ba05efe28392c2ec
alexandermcmillan <mcmillancontact@protonmail.com>
2024-10-13 20:13:25 -0700

    completed lab

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
+from sqlalchemy import desc
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
@@ -16,23 +16,80 @@ db.init_app(app)
 
 @app.route('/')
 def index():
+    
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    #make an empty list
+    bakeries = []
+    #loop db query to get all bakeries
+    for bakery in Bakery.query.all():
+        #build the body of the bakery dict
+        bakery_dict = {
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at
+        }
+        #append to the empty list
+        bakeries.append(bakery_dict)
+
+    #make the response
+    response = make_response(
+        jsonify(bakeries), 
+        200,
+        {"Content-Type": 'application/json'})
+    #return the response
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = {
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at
+        }
+
+    response = make_response(
+        jsonify(bakery_dict), 
+        200)
+    
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    prices = []
+    for good in BakedGood.query.order_by(desc(BakedGood.price)).all():
+        good_dict = {
+            'id': good.id,
+            'name': good.name,
+            'price': good.price,
+            'created_at': good.created_at
+        }
+        prices.append(good_dict)
+
+    response = make_response(prices, 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
+    good = BakedGood.query.order_by(desc(BakedGood.price)).first()
+    good_dict = {
+            'id': good.id,
+            'name': good.name,
+            'price': good.price,
+            'created_at': good.created_at
+        }
+
+    response = make_response(
+        jsonify(good_dict), 
+        200)
+    
+    return response
+    
+
     return ''
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5554, debug=True)
